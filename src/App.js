26fb9bfd5855df14f3a738cf5834c672977ab8ff
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-04-04 23:47:37 +0600

    configure firebase with app and store data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
 import Todo from './Components/Todo/Todo';
+import firebaseStore from './firebase';
+import firebase from 'firebase';
 
 import './App.css';
 
 function App() {
-	const [todos, setTodos] = useState([
-		'Take a show for walk',
-		'Take the rubbish and throw out',
-	]);
+	const [todos, setTodos] = useState([]);
 	const [input, setInput] = useState('');
+
+	// when the app loads, we need to listen to the database and fetch new todos when user added/removed
+	useEffect(() => {
+		// this code will fire when app.js loads (Read data from database)
+		firebaseStore
+			.collection('todos')
+			.orderBy('timestamp', 'desc')
+			.onSnapshot((snapshot) => {
+				setTodos(snapshot.docs.map((doc) => doc.data().todo));
+			});
+	}, []);
 
 	const addTodo = (event) => {
 		// Will stop the refresh
 		event.preventDefault();
+
+		// Store todo item in to database that inputed by users
+		firebaseStore.collection('todos').add({
+			todo: input,
+			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+		});
 
 		// It will be fire of when we will click on the button
 		setTodos([...todos, input]);
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+	apiKey: 'AIzaSyDJt2XVV_ogtNdSwUO5-pufx8l3O2Vgz98',
+	authDomain: 'todoapp-e36ab.firebaseapp.com',
+	projectId: 'todoapp-e36ab',
+	storageBucket: 'todoapp-e36ab.appspot.com',
+	messagingSenderId: '928959702475',
+	appId: '1:928959702475:web:248b702d4d760c13b2c24e',
+	measurementId: 'G-4GKED50HER',
+});
+
+const firebaseStore = firebaseApp.firestore();
+
+export default firebaseStore;
